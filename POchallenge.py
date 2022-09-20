62841171bb7d433c10da98743af711d3f700cf16
Kvergara02 <103055166+Kvergara02@users.noreply.github.com>
2022-09-20 10:42:02 -0400

    Update POchallenge.py

--- POchallenge.py
+++ POchallenge.py
@@ -1,9 +1,6 @@
 x =list(input("Please input numbers: "))
-for i in range(len(x)):
-    i = i**3
-    print(i)
-if i == x:
-    print(sorted(x)[-2])
+x.sort()
+print(sorted(x) [-2])
 
 
 print("New Problem")
